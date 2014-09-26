59c21c4ac3e42c38083794e0970d6c4af9e8664c
Leigh Halliday <lhalliday@bettertheworld.com>
2014-09-26 07:36:27 -0400

    Rename recursive method

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -4,8 +4,8 @@ class Hamming
     [a.size, b.size].min.times.count { |i| a[i] != b[i] }
   end
 
-  def self.compute_a(a, b)
-    (a[0] == nil || b[0] == nil) ? 0 : (a[0] == b[0] ? 0 : 1) + compute(a[1..-1], b[1..-1])
+  def self.compute_recur(a, b)
+    (a[0] == nil || b[0] == nil) ? 0 : (a[0] == b[0] ? 0 : 1) + compute_recur(a[1..-1], b[1..-1])
   end
 
 end
