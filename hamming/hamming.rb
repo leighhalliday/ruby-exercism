80ca58f8a049688faa82877f5a621eb47a748191
Leigh Halliday <lhalliday@bettertheworld.com>
2014-09-26 07:34:58 -0400

    Hamming

---
+++ hamming/hamming.rb
@@ -0,0 +1,11 @@
+class Hamming
+
+  def self.compute(a, b)
+    [a.size, b.size].min.times.count { |i| a[i] != b[i] }
+  end
+
+  def self.compute_a(a, b)
+    (a[0] == nil || b[0] == nil) ? 0 : (a[0] == b[0] ? 0 : 1) + compute(a[1..-1], b[1..-1])
+  end
+
+end
