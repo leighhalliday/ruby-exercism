9dddb41fe95bd63ede62d14da7df98261fb3a767
Leigh Halliday <lhalliday@bettertheworld.com>
2014-10-17 09:22:40 -0400

    Bob.

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,5 +1,33 @@
 class Bob
   def hey(phrase)
-    ""
+    @remark = Remark.new(phrase)
+    case
+    when @remark.silence?
+      "Fine. Be that way!"
+    when @remark.yell?
+      "Woah, chill out!"
+    when @remark.question?
+      "Sure."
+    else
+      "Whatever."
+    end
+  end
+end
+
+class Remark
+  def initialize(phrase)
+    @phrase = phrase.to_s.strip
+  end
+
+  def silence?
+    @phrase.empty?
+  end
+
+  def question?
+    @phrase.end_with?("?")
+  end
+
+  def yell?
+    @phrase.upcase == @phrase
   end
 end
