5ef8744e2232278d6d491af4d8c570dcc92e2a2a
Leigh Halliday <lhalliday@bettertheworld.com>
2014-10-17 13:13:13 -0400

    Further refactoring

--- rna-transcription/complement.rb
+++ rna-transcription/complement.rb
@@ -1,16 +1,12 @@
 class Complement
-  DNA_MAPPINGS = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
-  RNA_MAPPINGS = DNA_MAPPINGS.invert
+  DNA_MAPPINGS = "GCTA"
+  RNA_MAPPINGS = "CGAU"
 
   def self.of_dna(strand)
-    map_strand(strand, DNA_MAPPINGS)
+    strand.tr(DNA_MAPPINGS, RNA_MAPPINGS)
   end
 
   def self.of_rna(strand)
-    map_strand(strand, RNA_MAPPINGS)
-  end
-
-  def self.map_strand(strand, mappings)
-    strand.tr(mappings.keys.join(""), mappings.values.join(""))
+    strand.tr(RNA_MAPPINGS, DNA_MAPPINGS)
   end
 end
