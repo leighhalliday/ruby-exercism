bbc63574734156fa179d7caafb4e673a009d9316
Leigh Halliday <lhalliday@bettertheworld.com>
2014-10-17 13:08:18 -0400

    Refactoring rna transcription

--- rna-transcription/complement.rb
+++ rna-transcription/complement.rb
@@ -1,30 +1,16 @@
 class Complement
-  def self.of_dna(dna_strand)
-    dna_strand.split("").map{|dna_nuc| map_dna_nuc_to_rna_nuc(dna_nuc)}.join("")
+  DNA_MAPPINGS = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+  RNA_MAPPINGS = DNA_MAPPINGS.invert
+
+  def self.of_dna(strand)
+    map_strand(strand, DNA_MAPPINGS)
   end
 
-  def self.of_rna(rna_strand)
-    rna_strand.split("").map{|rna_nuc| map_rna_nuc_to_dna_nuc(rna_nuc)}.join("")
+  def self.of_rna(strand)
+    map_strand(strand, RNA_MAPPINGS)
   end
 
-  def self.dna_mappings
-    {
-      'G' => 'C',
-      'C' => 'G',
-      'T' => 'A',
-      'A' => 'U'
-    }
-  end
-
-  def self.rna_mappings
-    dna_mappings.invert
-  end
-
-  def self.map_dna_nuc_to_rna_nuc(dna_nuc)
-    dna_mappings[dna_nuc]
-  end
-
-  def self.map_rna_nuc_to_dna_nuc(rna_nuc)
-    rna_mappings[rna_nuc]
+  def self.map_strand(strand, mappings)
+    strand.tr(mappings.keys.join(""), mappings.values.join(""))
   end
 end
